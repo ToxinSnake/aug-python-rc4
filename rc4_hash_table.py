0ab02fa3b9733ffc8c29c65669e1de148bda1c9f
Marc Pollmann <marc-pollmann@msn.com>
2019-04-19 15:42:08 +0200

    Prüfung der Abdeckung des Passwortraumes

--- rc4_hash_table.py
+++ rc4_hash_table.py
@@ -4,6 +4,9 @@ import sys
 from random import randint
 import rc4_hash
 
+
+allHashes = set()
+
 def rand_password():
     h = ""
     for i in range(3):               
@@ -19,7 +22,8 @@ def generate_start_values(quantity):
 
 
 def generate_hashes(valueList, quantity):
-    hashes = list()
+    global allHashes
+    hashes = list()    
     
     lenValList = len(valueList)
     status = int(lenValList/20)
@@ -32,16 +36,17 @@ def generate_hashes(valueList, quantity):
             hash = rc4_hash.convertPass(hash)
             hash = rc4_hash.prga(rc4_hash.ksa(hash))
             hash = rc4_hash.arraytoHexString(hash)
-        hashes.append(hash)
+            allHashes.add(hash)
+        hashes.append(hash)        
         #zwischendurch Status drucken 
         if (len(hashes) % status == 0):
             print(str(int(len(hashes)/lenValList*100))+"% ("+str(len(hashes))+" values)")
     return hashes
 
 
-def writeFile(filename, list1, list2):
+def writeFile(filename, list1, list2, distinctHashes, coverage):
     file = open(filename, "w")
-    file.write("PASSWD  HASH  ("+str(len(list1))+" entries)\n---------------------------------\n")
+    file.write("PASSWD  HASH  ("+str(len(list1))+" entries, "+str(distinctHashes)+" distinct hashes (coverage:"+str(coverage)+" %))\n---------------------------------\n")
     i=0
     for value in list1:
        file.write(value+" "+list2[i]+"\n")
@@ -63,17 +68,23 @@ if __name__== "__main__":
     
     print("N =",N,"\nn =",n,"\nm =",m)    
     
-    print("\n(1/3) generate start values ...")
+    print("\n(1/4) generate start values ...")
     startValues = generate_start_values(n)
     print("- done")
     #print(startValues)
     
-    print("\n(2/3) create hash chains ...")
+    print("\n(2/4) create hash chains ...")
     hashList = generate_hashes(startValues, m)
-    print("- done")
+    print("- done")    
+    
+    print("\n(3/4) check coverage ...")
+    distinctHashes = len(allHashes)
+    coverage = distinctHashes/N*100
+    print("generated "+str(distinctHashes)+" distinct hashes")
+    print("coverage: "+str(coverage)+" %")    
     #print(hashList)
     
-    print("\n(3/3) write data to file ...")
-    writeFile("table1.txt",startValues,hashList)
+    print("\n(4/4) write data to file ...")
+    writeFile("table1.txt",startValues,hashList,distinctHashes,coverage)
     print("- done")
     
